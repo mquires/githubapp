2ae5d49d32f680daf5882bee4aad0024ed54ee79
mquires <evg.tulya@gmail.com>
2022-09-06 11:39:41 +0300

    Initial Commit

---
+++ githubapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubapp
+//
+//  Created by wsa-151-41b on 6.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
